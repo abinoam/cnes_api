116ea964594dae0111c2d50c4500994214523c43
Peter Leiva <pleiva@id.uff.br>
2016-07-10 18:54:26 -0300

    Modifica a aplicação para API Only do Rails

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module CnesApi
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.api_only = true
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.debug_exception_response_format = :default
+
+  config.debug_exception_response_format = :api
 end
