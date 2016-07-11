e8c81a788221fb801c15d9ce1602570d8a0956a0
Peter Leiva <pleiva@id.uff.br>
2016-07-11 18:23:28 -0300

    Retorna estabelecimentos de saúde na api
    A api retorna os estabelecimentos de saúde pelo recurso /api/cnes. A Api
    apenas retorna alguns dados para amostra no webapp

--- Gemfile
+++ Gemfile
@@ -7,6 +7,9 @@ gem 'rails', '4.2.6'
 # gem 'sqlite3'
 # Usa postgresql como banco de dados para Active Record
 gem 'pg'
+# Usa json
+gem 'json'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/api/cnes_controller.rb
+++ app/controllers/api/cnes_controller.rb
@@ -1,4 +1,34 @@
+require "json"
+
 class Api::CnesController < ApplicationController
+  JSON_FILE = 'public/estabelecimentos.json'
+
   def index
+    health_care = load_json JSON_FILE
+    arr = []
+    arr << health_care['COLUMNS']
+    50.times do |i|
+      arr << health_care['DATA'][i]
+    end
+
+    render json: arr
   end
+
+  private
+    ##
+    # Lê os dados em JSON de um arquivo
+    #
+    ##
+    def load_json(file_name)
+      health_care_establishments_file = File.read file_name
+      health_care_establishments = JSON.parse health_care_establishments_file
+    end
+
+    ##
+    # Retorna um vetor com todos os estabelecimentos de saúde
+    #
+    ##
+    def readHealthCareEstablishments
+      
+    end
 end
