091a4a5299045be938359d8d2dc4ca3b4ce9d44a
Peter Leiva <pleiva@id.uff.br>
2016-07-20 09:26:12 -0300

    Modificado limite de dados do recurso principal

--- app/controllers/api/cnes_controller.rb
+++ app/controllers/api/cnes_controller.rb
@@ -4,7 +4,7 @@ class Api::CnesController < ApplicationController
   JSON_FILE = 'public/estabelecimentos.json'
 
   def index
-    render json: HealthCareEstablishment.all, include: json_options
+    render json: HealthCareEstablishment.first(500), include: json_options
   end
 
   def setup
